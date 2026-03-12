53daef578e1d23d5ca055e3767ccfbf9930fe81e
Adrianvvr <adrianvivarmo@gmail.com>
2026-03-12 11:51:29 +0100

    Estructura completa: Base de datos migrada y Tailwind funcionando

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-indigo-900">
+      <h1 className="text-5xl font-extrabold text-yellow-400 drop-shadow-lg">
+        ¡LootCloset Vive!
+      </h1>
+    </div>
+  )
+}
+
+export default App
